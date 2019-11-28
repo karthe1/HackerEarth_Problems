6d697d003c1e36d570b06d88eb5870de3c221737
karthe1 <karthickamala27@gmail.com>
2019-11-28 16:16:30 +0530

    File name has been renamed for Mp3PlayList problem.

--- src/com/karthick/practice/Mp3PlayList.java
+++ src/com/karthick/practice/Mp3PlayList.java
@@ -15,7 +15,7 @@ import static java.util.stream.Collectors.toList;
 /**
  * @author karthick.r1
  */
-public class Demo {
+public class Mp3PlayList {
 
     public static void main(String[] args) throws IOException {
 
